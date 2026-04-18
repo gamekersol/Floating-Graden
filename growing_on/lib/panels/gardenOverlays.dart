f4a01800ad60e6e4bf46b2253989a14c20a8bd59
gamekersol <121124234+gamekersol@users.noreply.github.com>
2026-04-18 21:13:37 +0300

    overlay size hotfix

--- growing_on/lib/panels/gardenOverlays.dart
+++ growing_on/lib/panels/gardenOverlays.dart
@@ -24,12 +24,15 @@ class _PhantomBlockWidget extends StatelessWidget {
   late Alignment _align;
   _PhantomBlockWidget({required this.block}){
     double oddYoffset = block.pos.x % 2 == 0 ? - blockAlignSize.y / 2 : 0;
-    _align = Alignment(block.pos.x * blockAlignSize.x, block.pos.y * blockAlignSize.y + oddYoffset);
+
+    // FIX THIS
+    _align = Alignment(block.pos.x * blockAlignSize.x * 0.95,
+    (block.pos.y * blockAlignSize.y + oddYoffset) * 0.9 - blockAlignSize.y * 0.3);
   }
   @override
   Widget build(BuildContext context) {
     return Align(
-      child: const Icon(Icons.add, size: 50,),
+      child: const Icon(Icons.add, size: 60,),
       alignment: _align,
     );
   }
