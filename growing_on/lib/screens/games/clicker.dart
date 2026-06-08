df0c1e18b627aa5e3f2969707dd2cdc4ccb26255
gamekersol <121124234+gamekersol@users.noreply.github.com>
2026-06-08 11:43:01 +0300

    circle transition , cost transaction

--- growing_on/lib/screens/games/clicker.dart
+++ growing_on/lib/screens/games/clicker.dart
@@ -25,9 +25,9 @@ class ClickerGame extends StatelessWidget {
           Center(child: clickCircle()),
 
           // UPGRADE OPTIONS
-          upgradeOptionWidget('Clicks', _requiredClicks),
+          upgradeOptionWidget('Clicks', _requiredClicks, 5 * (6 - _requiredClicks)),
          // SizedBox(height: 10),
-          upgradeOptionWidget('Cost', _cost),
+          upgradeOptionWidget('Cost', _cost, _cost * 20),
         ],
       ),
     );
@@ -38,10 +38,12 @@ class ClickerGame extends StatelessWidget {
     onTap: () => countDown(),
     child: ListenableBuilder(
       listenable: _counter,
-      builder: (context, child) => Container(
+      builder: (context, child) => AnimatedContainer(
+        duration: Duration(milliseconds: 150),
         alignment: .center,
-        width: 250,
-        height: 250,
+        margin: .all((150 - 150 / _counter.value)/2),
+        width: 100 + 150 / _counter.value,
+        height: 100 + 150 / _counter.value,
         decoration: ShapeDecoration(
           color: Colors.amber[100],
           shape: CircleBorder(side: BorderSide(color: Colors.yellow, width: 10))
@@ -51,7 +53,7 @@ class ClickerGame extends StatelessWidget {
     ),
   );
 
-  Widget upgradeOptionWidget(String paramName, Object value){
+  Widget upgradeOptionWidget(String paramName, Object value, int cost){
     return Expanded(
       flex: 1,
       child: Row(
@@ -97,7 +99,7 @@ class ClickerGame extends StatelessWidget {
           Expanded(
             flex: 2,
             child: IconButton(
-              onPressed: () {},
+              onPressed: () => currencys.change(.diamonds, -cost),
               icon: Container(
                 //padding: .all(50),
                 decoration: BoxDecoration(
@@ -108,7 +110,7 @@ class ClickerGame extends StatelessWidget {
                   mainAxisAlignment: .center,
                   spacing: 10,
                   children: [
-                    AutoSizeText(((value as int) * 6).toString()),
+                    AutoSizeText(cost.toString()),
                     SizedBox.square(dimension:  50, child: currencys.values[TypeOfCurrency.diamonds]!.getImage()),
                   ],
                 ),
